06c86d65505013d48eec44adc1a04d5d5077801b
AoKishuba <75347231+AoKishuba@users.noreply.github.com>
2021-04-02 17:21:13 -0700

    Update Module.cs

--- ApsCalc/Module.cs
+++ ApsCalc/Module.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -47,23 +47,24 @@ namespace ApsCalc
         public bool IsChem { get; }
 
         // Initialize every unique module type
-        public static Module SolidBody { get; } = new Module("Solid Body", 1.1f, 1.0f, 1.0f, 1.0f, 500, Position.Middle, false);
-        public static Module SabotBody { get; } = new Module("Sabot Body", 1.1f, 0.8f, 1.4f, 0.25f, 500, Position.Middle, false);
-        public static Module ChemBody { get; } = new Module("HE, Frag, FlaK, or EMP Body", 1.0f, 1.0f, 0.1f, 1.0f, 500, Position.Middle, true);
+        public static Module SolidBody { get; } = new Module("Solid body", 1.1f, 1.0f, 1.0f, 1.0f, 500, Position.Middle, false);
+        public static Module SabotBody { get; } = new Module("Sabot body", 1.1f, 0.8f, 1.4f, 0.25f, 500, Position.Middle, false);
+        public static Module ChemBody { get; } = new Module("HE, Frag, FlaK, or EMP body", 1.0f, 1.0f, 0.1f, 1.0f, 500, Position.Middle, true);
         public static Module FuseBody { get; } = new Module("Fuse", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Middle, false);
-        public static Module FinBody { get; } = new Module("Stabilizer Fin Body", 0.95f, 1.0f, 1.0f, 1.0f, 300, Position.Middle, false);
-        public static Module GravCompensator { get; } = new Module("Grav. Compensator", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Middle, false);
-        public static Module ChemHead { get; } = new Module("HE, Frag, FlaK, or EMP Head", 1.3f, 1.0f, 0.1f, 1.0f, 500, Position.Head, true);
-        public static Module SpecialHead { get; } = new Module("Squash or Shaped Charge Head", 1.45f, 0.1f, 0.1f, 1.0f, 500, Position.Head, true);
-        public static Module APHead { get; } = new Module("Armor Piercing Head", 1.6f, 1.0f, 1.65f, 1.0f, 500, Position.Head, false);
-        public static Module SabotHead { get; } = new Module("Sabot Head", 1.6f, 0.85f, 2.5f, 0.25f, 500, Position.Head, false);
-        public static Module HeavyHead { get; } = new Module("Heavy Head", 1.45f, 1.65f, 1.0f, 1.0f, 500, Position.Head, false);
-        public static Module SkimmerTip { get; } = new Module("Skimmer Tip", 1.6f, 1.0f, 1.4f, 1.0f, 500, Position.Head, false);
-        public static Module Disruptor { get; } = new Module("Disruptor Conduit", 1.6f, 1.0f, 0.1f, 1f, 500, Position.Head, true);
-        public static Module BaseBleeder { get; } = new Module("Base Bleeder", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Base, false);
-        public static Module Supercav { get; } = new Module("Supercavitation Base", 1.0f, 1.0f, 1.0f, 0.75f, 100, Position.Base, false);
-        public static Module Tracer { get; } = new Module("Visible Tracer", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Base, false);
-        public static Module GravRam { get; } = new Module("Graviton Ram", 1.0f, 1.0f, 1.0f, 1.0f, 500, Position.Base, false);
+        public static Module FinBody { get; } = new Module("Stabilizer fin body", 0.95f, 1.0f, 1.0f, 1.0f, 300, Position.Middle, false);
+        public static Module GravCompensator { get; } = new Module("Grav. compensator", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Middle, false);
+        public static Module ChemHead { get; } = new Module("HE, Frag, FlaK, or EMP head", 1.3f, 1.0f, 0.1f, 1.0f, 500, Position.Head, true);
+        public static Module SpecialHead { get; } = new Module("Squash or Shaped charge head", 1.45f, 0.1f, 0.1f, 1.0f, 500, Position.Head, true);
+        public static Module APHead { get; } = new Module("Armor piercing head", 1.6f, 1.0f, 1.65f, 1.0f, 500, Position.Head, false);
+        public static Module SabotHead { get; } = new Module("Sabot head", 1.6f, 0.85f, 2.5f, 0.25f, 500, Position.Head, false);
+        public static Module HeavyHead { get; } = new Module("Heavy head", 1.45f, 1.65f, 1.0f, 1.0f, 500, Position.Head, false);
+        public static Module HollowPoint { get; } = new Module("Hollow point head", 1.45f, 1.2f, 1.0f, 1.0f, 500, Position.Head, false);
+        public static Module SkimmerTip { get; } = new Module("Skimmer tip", 1.6f, 1.0f, 1.4f, 1.0f, 500, Position.Head, false);
+        public static Module Disruptor { get; } = new Module("Disruptor conduit", 1.6f, 1.0f, 0.1f, 1f, 500, Position.Head, true);
+        public static Module BaseBleeder { get; } = new Module("Base bleeder", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Base, false);
+        public static Module Supercav { get; } = new Module("Supercavitation base", 1.0f, 1.0f, 1.0f, 0.75f, 100, Position.Base, false);
+        public static Module Tracer { get; } = new Module("Visible tracer", 1.0f, 1.0f, 1.0f, 1.0f, 100, Position.Base, false);
+        public static Module GravRam { get; } = new Module("Graviton ram", 1.0f, 1.0f, 1.0f, 1.0f, 500, Position.Base, false);
 
         // List modules for reference
         public static Module[] AllModules { get; } =
@@ -79,6 +80,7 @@ namespace ApsCalc
         APHead,
         SabotHead,
         HeavyHead,
+        HollowPoint,
         SkimmerTip,
         Disruptor,
         BaseBleeder,
